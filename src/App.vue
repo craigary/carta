e741f39198baba9c4bf464f64d0fbcc267e96953
Craig Hart <i@craigary.net>
2021-05-07 08:23:53 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="prose container mx-auto px-4 mt-8">
+    <Carta />
+  </div>
+</template>
+
+<script>
+import Carta from './assets/carta.md'
+export default {
+  components: {
+    Carta
+  }
+}
+</script>
+
+<style></style>
---
+++ src/assets/carta.md
@@ -0,0 +1,7 @@
+# Carta
+
+Carta (Italian word for "paper", pronounced /càr·ta/) is a personal tool for me to generate a website for my projects by using markdown files.
+
+## License
+
+The MIT License.
